92612fe50c02a14b8330782b2d89a221ad4260fa
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-10-16 12:14:58 +0530

    resolve dockerBuild file errors.

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,4 +1,4 @@
 def call(string projectName, string projectTag){
-    echo 'Building Docker image...'
-    sh 'docker build -t ${projectName}:${projectTag} .'
+    echo "Building Docker image..."
+    sh "docker build -t ${projectName}:${projectTag} ."
 }
