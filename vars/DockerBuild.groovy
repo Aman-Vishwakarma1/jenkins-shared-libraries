c5ea26c024feeafe074f80f313717f934c511e27
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-10-16 12:18:33 +0530

    fixed systax error in dockerBuild and uploadToDockerHub files.

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,4 +1,4 @@
-def call(string projectName, string projectTag){
+def call(String projectName, String projectTag){
     echo "Building Docker image..."
     sh "docker build -t ${projectName}:${projectTag} ."
 }
--- vars/uploadToDockerHub.groovy
+++ vars/uploadToDockerHub.groovy
@@ -1,4 +1,4 @@
-def call(string dockerUserName, string dockerPass, string projectName, string projectTag){
+def call(String dockerUserName, String dockerPass, String projectName, String projectTag){
     echo "uploading docker image to DockerHub"
     sh "docker login -u ${dockerUserName} -p ${dockerPass}"
     sh "docker tag ${projectName} ${dockerUserName}/${projectName}:${projectTag}"
