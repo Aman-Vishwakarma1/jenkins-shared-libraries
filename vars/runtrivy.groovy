3c43d604976eb7879c7e3d12529ca29c354eddfb
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-10-27 03:53:09 +0530

    added trivy name update.

--- vars/runtrivy.groovy
+++ vars/runtrivy.groovy
@@ -1,7 +1,5 @@
 def call(){
-    steps{
         echo "Intiallizing trivy file System Check....."
         sh "trivy fs --format table -o trivy-fs-report.html ."
         echo "Completed trivy file System Check"
-    }
 }
