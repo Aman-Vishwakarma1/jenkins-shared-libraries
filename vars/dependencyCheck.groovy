8553f0661ee9cef13563639802ca4f921a4168fa
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-10-27 02:28:55 +0530

    added dep check  shared library.

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,6 +1,11 @@
-def call(String dependencyCheckInstalltionName){
-    echo "Initiallizing OWASP Dependency Check"
-    dependencyCheck additionalArguments: "--scan ./", odcInstallation:"${dependencyCheckInstalltionName}"
-    dependencyCheckPublisher pattern: "**/dependency-check-report.xml"
+def call(Map config = [:]) {
+    echo "Initializing OWASP Dependency Check"
+
+    def args = config.get('additionalArguments', '--scan ./')
+    def installation = config.get('odcInstallation', 'Default-OWASP')
+
+    dependencyCheck additionalArguments: args, odcInstallation: installation
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+
     echo "Completed OWASP Dependency Check"
 }
